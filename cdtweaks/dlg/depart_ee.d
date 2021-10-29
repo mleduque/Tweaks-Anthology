6ee4465ceebdd89563a0149cb5afb94800437771
CamDawg <pcamagna@yahoo.com>
2021-10-29 14:13:46 -0700

    SoD/EET resource review
    Found a number of small issues where components were patching the wrong resources or missing files on EET and SoD.

--- cdtweaks/dlg/depart_ee.d
+++ cdtweaks/dlg/depart_ee.d
@@ -1,13 +1,13 @@
-REPLACE_ACTION_TEXT ~neerap~   ~EscapeAreaMove("AR2301",755,390,SW)~  ~~
-REPLACE_ACTION_TEXT ~rasaadp~  ~EscapeAreaMove("AR4800",1100,782,S)~  ~~
-REPLACE_ACTION_TEXT ~dornp~    ~EscapeAreaMove("AR2301",1132,727,S)~  ~~
-ALTER_TRANS DORNP BEGIN 2 END BEGIN 0 END BEGIN ~JOURNAL~ ~~ END // remove journal entry about going to FAI
+REPLACE_ACTION_TEXT ~%NEERA_POST%~   ~EscapeAreaMove("%FriendlyArmInn_L1%",755,390,SW)~  ~~
+REPLACE_ACTION_TEXT ~%RASAAD_POST%~  ~EscapeAreaMove("%Nashkel%",1100,782,S)~  ~~
+REPLACE_ACTION_TEXT ~%DORN_POST%~    ~EscapeAreaMove("%FriendlyArmInn_L1%",1132,727,S)~  ~~
+ALTER_TRANS ~%DORN_POST%~ BEGIN 2 END BEGIN 0 END BEGIN ~JOURNAL~ ~~ END // remove journal entry about going to FAI
 
 // baeloth needs custom code since he's being sent to inn-adjacent areas
 
 // first, turn baeloth's existing departure to a simple 'stay here'; move already handled above
 ALTER_TRANS baelothp BEGIN 3 END BEGIN 0 END BEGIN ~REPLY~ ~@20850~ END 
-REPLACE_ACTION_TEXT ~baelothp~ ~EscapeAreaMove("AR2300",4721,3045,S)~ ~~
+REPLACE_ACTION_TEXT ~baelothp~ ~EscapeAreaMove("%FriendlyArmInn%",4721,3045,S)~ ~~
 
 // more or less copied from depart.d, but using custom replies since he's not quite going to the same places
 EXTEND_BOTTOM baelothp 0
@@ -30,8 +30,8 @@ IF ~Global("EnteredArmInn","GLOBAL",1)
     !AreaCheck("%IceIsland%")                        // ice isle surface
     !AreaCheck("%IceIslandMaze_L1%")                 // ice isle cavern 1
     !AreaCheck("%IceIslandMaze_L2%")                 // ice isle cavern 2
-    !AreaCheck("AR2300")~                            // not in the area where the NPC will be sent
-THEN REPLY #31931 GOTO dmww_fai
+    !AreaCheck("%FriendlyArmInn%")~                  // not in the area where the NPC will be sent
+THEN REPLY #%baelothreply% GOTO dmww_fai
 
 IF ~Global("EnteredBeregost","GLOBAL",1)
     Global("cd_no_travel_jugg_all","GLOBAL",0)
@@ -52,7 +52,7 @@ IF ~Global("EnteredBeregost","GLOBAL",1)
     !AreaCheck("%IceIsland%")                        // ice isle surface
     !AreaCheck("%IceIslandMaze_L1%")                 // ice isle cavern 1
     !AreaCheck("%IceIslandMaze_L2%")                 // ice isle cavern 2
-    !AreaCheck("AR3300")~                            // not in the area where the NPC will be sent
+    !AreaCheck("%Beregost%")~                        // not in the area where the NPC will be sent
 THEN REPLY @20848  GOTO dmww_beregost
 
 IF ~GlobalGT("Chapter","GLOBAL",1)
@@ -74,7 +74,7 @@ IF ~GlobalGT("Chapter","GLOBAL",1)
     !AreaCheck("%IceIsland%")                        // ice isle surface
     !AreaCheck("%IceIslandMaze_L1%")                 // ice isle cavern 1
     !AreaCheck("%IceIslandMaze_L2%")                 // ice isle cavern 2
-    !AreaCheck("AR4800")~                            // not in the area where the NPC will be sent
+    !AreaCheck("%Nashkel%")~                         // not in the area where the NPC will be sent
 THEN REPLY @20847  GOTO dmww_nash
 
 IF ~OR(2)
@@ -99,7 +99,7 @@ IF ~OR(2)
     !AreaCheck("%IceIsland%")                        // ice isle surface
     !AreaCheck("%IceIslandMaze_L1%")                 // ice isle cavern 1
     !AreaCheck("%IceIslandMaze_L2%")                 // ice isle cavern 2
-    !AreaCheck("AR0800")~                            // not in the area where the NPC will be sent
+    !AreaCheck("%EBaldursGate%")~                    // not in the area where the NPC will be sent
 THEN REPLY @20849  GOTO dmww_elfsong
 END
 
@@ -107,7 +107,7 @@ APPEND baelothp
 IF ~~ THEN BEGIN dmww_fai
   SAY @20851
   IF ~~ THEN DO ~RunAwayFromNoInterrupt([PC],120)
-  Face(0) MoveGlobal("AR2300","baeloth",[4721.3045])~
+  Face(0) MoveGlobal("%FriendlyArmInn%","baeloth",[4721.3045])~
 EXIT
 END
 
@@ -115,7 +115,7 @@ IF
   ~~ THEN BEGIN dmww_beregost
   SAY @20851
   IF ~~ THEN DO ~RunAwayFromNoInterrupt([PC],120)
-  Face(0) MoveGlobal("ar3300","baeloth",[829.2427])~
+  Face(0) MoveGlobal("%Beregost%","baeloth",[829.2427])~
 EXIT
 END
 
@@ -123,7 +123,7 @@ IF
   ~~ THEN BEGIN dmww_nash
   SAY @20851
   IF ~~ THEN DO ~RunAwayFromNoInterrupt([PC],120)
-  Face(0) MoveGlobal("ar4800","baeloth",[432.1288])~
+  Face(0) MoveGlobal("%Nashkel%","baeloth",[432.1288])~
 EXIT
 END
 
@@ -131,7 +131,7 @@ IF
   ~~ THEN BEGIN dmww_elfsong
   SAY @20851
   IF ~~ THEN DO ~RunAwayFromNoInterrupt([PC],120)
-  Face(0) MoveGlobal("ar0800","baeloth",[353.1623])~
+  Face(0) MoveGlobal("%EBaldursGate%","baeloth",[353.1623])~
 EXIT
 END
 
