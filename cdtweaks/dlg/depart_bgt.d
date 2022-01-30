84d8a82b8e99c22cc7bea2e7a3daa7deb7a98ec6
CamDawg <pcamagna@yahoo.com>
2022-01-30 08:59:22 -0800

    BGT vs. NPCs to an Inn, pairs separating, take 817

--- cdtweaks/dlg/depart_bgt.d
+++ cdtweaks/dlg/depart_bgt.d
@@ -1,6 +1,6 @@
 // bgt adds actions to send J&K back to the FAI, something tutu/bgee/eet don't have
-REPLACE_ACTION_TEXT ~%JAHEIRA_POST%~ ~ActionOverride("Khalid",EscapeAreaMove("%FriendlyArmInn_L1%",328,656,14))~ ~~
-REPLACE_ACTION_TEXT ~%JAHEIRA_POST%~ ~EscapeAreaMove("%FriendlyArmInn_L1%",315,711,13)~  ~~
+REPLACE_ACTION_TEXT ~%JAHEIRA_POST%~ ~ActionOverride[ %TAB%]*([ %TAB%]*"[Kk][Hh][Aa][Ll][Ii][Dd]"[ %TAB%]*,[ %TAB%]*EscapeAreaMove[ %TAB%]*([ %TAB%]*"%FriendlyArmInn_L1%"[ %TAB%]*,[ %TAB%]*[0-9]+[ %TAB%]*,[ %TAB%]*[0-9]+[ %TAB%]*,[ %TAB%]*[0-9NSEW]+[ %TAB%]*)[ %TAB%]*)~ ~~
+REPLACE_ACTION_TEXT ~%JAHEIRA_POST%~ ~EscapeAreaMove[ %TAB%]*([ %TAB%]*"%FriendlyArmInn_L1%"[ %TAB%]*,[ %TAB%]*[0-9]+[ %TAB%]*,[ %TAB%]*[0-9]+[ %TAB%]*,[ %TAB%]*[0-9NSEW]+[ %TAB%]*)~  ~~
 
-REPLACE_ACTION_TEXT ~%KHALID_POST%~ ~ActionOverride("Jaheira",EscapeAreaMove("%FriendlyArmInn_L1%",315,711,13))~ ~~
-REPLACE_ACTION_TEXT ~%KHALID_POST%~ ~EscapeAreaMove("%FriendlyArmInn_L1%",328,656,14)~  ~~
+REPLACE_ACTION_TEXT ~%KHALID_POST%~ ~ActionOverride[ %TAB%]*([ %TAB%]*"[Ja][Aa][Hh][Ee][Ii][Rr][Aa]"[ %TAB%]*,[ %TAB%]*EscapeAreaMove[ %TAB%]*([ %TAB%]*"%FriendlyArmInn_L1%"[ %TAB%]*,[ %TAB%]*[0-9]+[ %TAB%]*,[ %TAB%]*[0-9]+[ %TAB%]*,[ %TAB%]*[0-9NSEW]+[ %TAB%]*)[ %TAB%]*)~ ~~
+REPLACE_ACTION_TEXT ~%KHALID_POST%~ ~EscapeAreaMove[ %TAB%]*([ %TAB%]*"%FriendlyArmInn_L1%"[ %TAB%]*,[ %TAB%]*[0-9]+[ %TAB%]*,[ %TAB%]*[0-9]+[ %TAB%]*,[ %TAB%]*[0-9NSEW]+[ %TAB%]*)~  ~~
