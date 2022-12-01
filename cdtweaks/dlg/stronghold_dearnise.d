deebc0a7d8a9563cd8f20bf3a7f135aa3fc42301
CamDawg <pcamagna@yahoo.com>
2022-12-01 12:43:15 -0800

    Remove dupe reply options for de'Arnise Keep with multi-strongholds on BG2EE/EET

--- cdtweaks/dlg/stronghold_dearnise.d
+++ cdtweaks/dlg/stronghold_dearnise.d
@@ -1,6 +1,8 @@
 REPLACE_TRIGGER_TEXT ~nalia~    ~Class(Player1,FIGHTER\(_[A-Z]+\)?)~ ~True()~
+REPLACE_TRIGGER_TEXT ~nalia~    ~Kit(Player1,Blackguard)~ ~True()~ // bg2ee
 
 REPLACE_TRIGGER_TEXT ~naliaj~   ~Class(Player1,FIGHTER\(_[A-Z]+\)*)~ ~True()~
+REPLACE_TRIGGER_TEXT ~naliaj~   ~Kit(Player1,Blackguard)~ ~True()~ // bg2ee
 
 ADD_TRANS_ACTION demson 
 BEGIN 119 END
