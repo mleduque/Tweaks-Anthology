da09c056a32c68d97b2352aa1ee6e0592817f3f0
4Luke4 <luke9300@icloud.com>
2024-01-07 10:50:23 +0100

    Minor tweaks

--- cdtweaks/luke/lua/spontaneous_casting.lua
+++ cdtweaks/luke/lua/spontaneous_casting.lua
@@ -1,15 +1,15 @@
 -- Decrement by 1 all memorized divine spells of level spellLevel (level specified by param1 of op402) --
 
-function GTSPCST1(effect, sprite) -- args: CGameEffect, CGameSprite
+function GTSPCST1(CGameEffect, CGameSprite)
 	-- CGameEffect (this effect, i.e. op402)
-	local spellLevel = effect.m_effectAmount -- param1 of op402
-	-- CGameSprite (the target object this effect is attached to)
-	local spellLevelMemListArray = sprite.m_memorizedSpellsPriest
+	local spellLevel = CGameEffect.m_effectAmount -- param1 of op402
+	-- CGameSprite (the target object op402 is attached to)
+	local spellLevelMemListArray = CGameSprite.m_memorizedSpellsPriest
 	-- Get all clerical spells of level spellLevel
 	local memList = spellLevelMemListArray:getReference(spellLevel - 1) -- level starts from 0!!!
 	local alreadyDecreasedResrefs = {}
 	-- Initialize my custom var to 0
-	EEex_Sprite_SetLocalInt(sprite, "cdtweaksSpontaneousCasting", 0)
+	EEex_Sprite_SetLocalInt(CGameSprite, "cdtweaksSpontaneousCasting", 0)
 	-- Cycle through all memorized divine spells of level spellLevel
 	EEex_Utility_IterateCPtrList(memList, function(memInstance)
 		local memInstanceResref = memInstance.m_spellId:get() -- We need to use :get() to export a CResRef field as a Lua string!
@@ -18,7 +18,7 @@ function GTSPCST1(effect, sprite) -- args: CGameEffect, CGameSprite
 			if EEex_IsBitSet(memFlags, 0) then
 				memInstance.m_flags = EEex_UnsetBit(memFlags, 0)
 				-- Set my custom var to 1, meaning that at least one divine spell of level spellLevel is memorized
-				EEex_Sprite_SetLocalInt(sprite, "cdtweaksSpontaneousCasting", 1)
+				EEex_Sprite_SetLocalInt(CGameSprite, "cdtweaksSpontaneousCasting", 1)
 				-- Make sure the engine catches the change
 				EEex_RunWithStackManager({
 					{ ["name"] = "abilityId", ["struct"] = "CAbilityId" } },
@@ -27,7 +27,7 @@ function GTSPCST1(effect, sprite) -- args: CGameEffect, CGameSprite
 						abilityId.m_itemType = 1 -- spell, not an item
 						abilityId.m_res:set(memInstanceResref)
 						-- CAbilityId* ab, short changeAmount, int remove, int removeSpellIfZero
-						sprite:CheckQuickLists(abilityId, -1, 0, 0)
+						CGameSprite:CheckQuickLists(abilityId, -1, 0, 0)
 					end
 				)
 				alreadyDecreasedResrefs[memInstanceResref] = true
@@ -38,16 +38,18 @@ end
 
 -- Check if the caster has at least one divine spell of level spellLevel memorized (feedback string specified by param1 of op402) --
 
-function GTSPCST2(effect, sprite) -- args: CGameEffect, CGameSprite
-	local parentResRef = effect.m_sourceRes:get() -- We need to use :get() to export a CResRef field as a Lua string!
-	local feedbackString = effect.m_effectAmount -- param1 of op402
+function GTSPCST2(CGameEffect, CGameSprite)
+	local parentResRef = CGameEffect.m_sourceRes:get() -- We need to use :get() to export a CResRef field as a Lua string!
+	local feedbackString = CGameEffect.m_effectAmount -- param1 of op402
 	--
-	if EEex_Sprite_GetLocalInt(sprite, "cdtweaksSpontaneousCasting") ~= 1 then
-		EEex_GameObject_ApplyEffect(sprite,
+	if EEex_Sprite_GetLocalInt(CGameSprite, "cdtweaksSpontaneousCasting") ~= 1 then
+		EEex_GameObject_ApplyEffect(CGameSprite,
 		{
 			["effectID"] = 206, -- Protection from spell
 			["effectAmount"] = feedbackString,
 			["res"] = parentResRef,
+			["sourceID"] = CGameEffect.m_sourceId, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
+			["sourceTarget"] = CGameEffect.m_sourceTarget, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
 		})
 	end
 end
