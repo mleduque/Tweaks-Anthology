6e50ed1722227150375f6cda28915ed0221d65a8
4Luke4 <luke9300@icloud.com>
2024-02-29 10:33:44 +0100

    Update dual_wield.lua

--- cdtweaks/luke/lua/dual_wield.lua
+++ cdtweaks/luke/lua/dual_wield.lua
@@ -1,14 +1,61 @@
 -- cdtweaks, dual-wield feat for rangers: Force the targeted creature to wield light armors (or no armor) in order to benefit from Two-Weapon Style --
 
-function GTDLWLD(CGameEffect, CGameSprite)
-	local equipment = CGameSprite.m_equipment -- CGameSpriteEquipment
-	local selectedItem = equipment.m_items:get(equipment.m_selectedWeapon) -- CItem
-	local itemHeader = selectedItem.pRes.pHeader -- Item_Header_st
+EEex_Opcode_AddListsResolvedListener(function(sprite)
+	-- internal function that applies the actual penalty
+	local apply = function(spriteProficiency2Weapon)
+		-- Update var
+		sprite:setLocalInt("cdtweaksDualWieldHelper", spriteProficiency2Weapon)
+		--
+		local stylbonu = GT_Resource_2DA["stylbonu"]
+		local maxThac0RightPenalty = tonumber(stylbonu["TWOWEAPON-0"]["THAC0_RIGHT"])
+		local maxThac0LeftPenalty = tonumber(stylbonu["TWOWEAPON-0"]["THAC0_LEFT"])
+		local curThac0RightPenalty = tonumber(stylbonu[string.format("TWOWEAPON-%s", spriteProficiency2Weapon)]["THAC0_RIGHT"])
+		local curThac0LeftPenalty = tonumber(stylbonu[string.format("TWOWEAPON-%s", spriteProficiency2Weapon)]["THAC0_LEFT"])
+		-- Mark the creature as 'malus applied'
+		sprite:setLocalInt("cdtweaksDualWield", 1)
+		--
+		sprite:applyEffect({
+			["effectID"] = 321, -- Remove effects by resource
+			["durationType"] = 1,
+			["res"] = "CDDLWLD",
+			["sourceID"] = sprite.m_id,
+			["sourceTarget"] = sprite.m_id,
+		})
+		sprite:applyEffect({
+			["effectID"] = 306, -- Main-hand THAC0 bonus
+			["durationType"] = 9,
+			["effectAmount"] = curThac0RightPenalty - maxThac0RightPenalty,
+			["m_sourceRes"] = "CDDLWLD",
+			["sourceID"] = sprite.m_id,
+			["sourceTarget"] = sprite.m_id,
+		})
+		sprite:applyEffect({
+			["effectID"] = 305, -- Off-hand THAC0 bonus
+			["durationType"] = 9,
+			["effectAmount"] = curThac0LeftPenalty - maxThac0LeftPenalty,
+			["m_sourceRes"] = "CDDLWLD",
+			["sourceID"] = sprite.m_id,
+			["sourceTarget"] = sprite.m_id,
+		})
+		sprite:applyEffect({
+			["effectID"] = 142, -- Main-hand THAC0 bonus
+			["durationType"] = 9,
+			["dwFlags"] = %feedback_icon%,
+			["m_sourceRes"] = "CDDLWLD",
+			["sourceID"] = sprite.m_id,
+			["sourceTarget"] = sprite.m_id,
+		})
+	end
+	-- Check creature's equipment / class
+	local equipment = sprite.m_equipment
+	local selectedItem = equipment.m_items:get(equipment.m_selectedWeapon)
+	local itemHeader = selectedItem.pRes.pHeader
+	--
 	local mainHandFlags = itemHeader.itemFlags
-	local itemAbility = EEex_PtrToUD(EEex_UDToPtr(itemHeader) + itemHeader.abilityOffset + Item_ability_st.sizeof * equipment.m_selectedWeaponAbility, "Item_ability_st") -- Item_ability_st
+	local itemAbility = EEex_Resource_GetItemAbility(itemHeader, equipment.m_selectedWeaponAbility) -- Item_ability_st
 	local mainHandAbilityType = itemAbility.type
 	--
-	local items = CGameSprite.m_equipment.m_items -- Array<CItem*,39>
+	local items = sprite.m_equipment.m_items -- Array<CItem*,39>
 	--
 	local armor = items:get(1) -- CItem (index from "slots.ids")
 	local armorType = nil
@@ -26,71 +73,42 @@ function GTDLWLD(CGameEffect, CGameSprite)
 		offHandType = itemHeader.itemType
 	end
 	--
-	local spriteProficiency2Weapon = CGameSprite.m_derivedStats.m_nProficiency2Weapon + CGameSprite.m_bonusStats.m_nProficiency2Weapon
+	local class = GT_Resource_IDSToSymbol["class"]
+	local spriteClass = sprite.m_typeAI.m_Class
 	--
-	local stylbonu = EEex_Resource_Load2DA("STYLBONU")
-	local maxThac0RightPenalty = tonumber(EEex_Resource_GetAt2DALabels(stylbonu, "THAC0_RIGHT", "TWOWEAPON-0"))
-	local maxThac0LeftPenalty = tonumber(EEex_Resource_GetAt2DALabels(stylbonu, "THAC0_LEFT", "TWOWEAPON-0"))
-	local curThac0RightPenalty = tonumber(EEex_Resource_GetAt2DALabels(stylbonu, "THAC0_RIGHT", string.format("TWOWEAPON-%s", spriteProficiency2Weapon)))
-	local curThac0LeftPenalty = tonumber(EEex_Resource_GetAt2DALabels(stylbonu, "THAC0_LEFT", string.format("TWOWEAPON-%s", spriteProficiency2Weapon)))
-	-- reset var if either curThac0RightPenalty or curThac0LeftPenalty changes
-	if curThac0RightPenalty ~= CGameEffect.m_effectAmount2 or curThac0LeftPenalty ~= CGameEffect.m_effectAmount3 then
-		EEex_Sprite_SetLocalInt(CGameSprite, "cdtweaksDualWield", -1)
-		CGameEffect.m_effectAmount2 = curThac0RightPenalty -- store curThac0RightPenalty in param#3
-		CGameEffect.m_effectAmount3 = curThac0LeftPenalty -- store curThac0RightPenalty in param#4
-	end
-	-- if the character is dual-wielding and is equipped with a medium or heavy armor ...
-	if not EEex_IsBitSet(mainHandFlags, 0x1) and mainHandAbilityType == 1 and offHand and offHandType ~= 0xC and armor and armorType == 0x2 and (armorAnimation == "3A" or armorAnimation == "4A") then
-		if EEex_Sprite_GetLocalInt(CGameSprite, "cdtweaksDualWield") ~= 1 then
-			EEex_Sprite_SetLocalInt(CGameSprite, "cdtweaksDualWield", 1)
-			--
-			EEex_GameObject_ApplyEffect(CGameSprite,
-			{
-				["effectID"] = 321, -- Remove effects by resource
-				["durationType"] = 1,
-				["res"] = "CDDLWLD",
-				["sourceID"] = CGameEffect.m_sourceId, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
-				["sourceTarget"] = CGameEffect.m_sourceTarget, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
-			})
-			EEex_GameObject_ApplyEffect(CGameSprite,
-			{
-				["effectID"] = 306, -- Main-hand THAC0 bonus
-				["effectAmount"] = curThac0RightPenalty - maxThac0RightPenalty,
-				["durationType"] = 9,
-				["m_sourceRes"] = "CDDLWLD",
-				["sourceID"] = CGameEffect.m_sourceId, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
-				["sourceTarget"] = CGameEffect.m_sourceTarget, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
-			})
-			EEex_GameObject_ApplyEffect(CGameSprite,
-			{
-				["effectID"] = 305, -- Off-hand THAC0 bonus
-				["effectAmount"] = curThac0LeftPenalty - maxThac0LeftPenalty,
-				["durationType"] = 9,
-				["m_sourceRes"] = "CDDLWLD",
-				["sourceID"] = CGameEffect.m_sourceId, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
-				["sourceTarget"] = CGameEffect.m_sourceTarget, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
-			})
-			EEex_GameObject_ApplyEffect(CGameSprite,
-			{
-				["effectID"] = 142, -- Display portrait icon
-				["dwFlags"] = %feedback_icon%,
-				["durationType"] = 9,
-				["m_sourceRes"] = "CDDLWLD",
-				["sourceID"] = CGameEffect.m_sourceId, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
-				["sourceTarget"] = CGameEffect.m_sourceTarget, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
-			})
+	local itemflag = GT_Resource_SymbolToIDS["itemflag"]
+	--
+	local itemcat = GT_Resource_IDSToSymbol["itemcat"]
+	-- If the Ranger is dual-wielding and is equipped with medium or heavy armor...
+	local applyCondition = EEex_IsBitUnset(mainHandFlags, itemflag["TWOHANDED"])
+		and mainHandAbilityType == 1 -- type: melee
+		and offHand and itemcat[offHandType] ~= "SHIELD"
+		and armor and itemcat[armorType] == "ARMOR" and (armorAnimation == "3A" or armorAnimation == "4A")
+		and (class[spriteClass] == "RANGER" or class[spriteClass] == "CLERIC_RANGER")
+	--
+	if sprite:getLocalInt("cdtweaksDualWield") == 0 then
+		if applyCondition then
+			apply(sprite.m_derivedStats.m_nProficiency2Weapon) -- since ``EEex_Opcode_AddListsResolvedListener`` is running after the effect lists have been evaluated, ``m_bonusStats`` has already been added to ``m_derivedStats`` by the engine
 		end
 	else
-		if EEex_Sprite_GetLocalInt(CGameSprite, "cdtweaksDualWield") ~= 0 then
-			EEex_Sprite_SetLocalInt(CGameSprite, "cdtweaksDualWield", 0)
-			EEex_GameObject_ApplyEffect(CGameSprite,
-			{
+		if applyCondition then
+			-- Check if ``m_nProficiency2Weapon`` has changed since the last application
+			local spriteProficiency2Weapon = sprite.m_derivedStats.m_nProficiency2Weapon
+			--
+			if spriteProficiency2Weapon ~= sprite:getLocalInt("cdtweaksDualWieldHelper") then
+				apply(spriteProficiency2Weapon)
+			end
+		else
+			-- Mark the creature as 'malus removed'
+			sprite:setLocalInt("cdtweaksDualWield", 0)
+			--
+			sprite:applyEffect({
 				["effectID"] = 321, -- Remove effects by resource
 				["durationType"] = 1,
 				["res"] = "CDDLWLD",
-				["sourceID"] = CGameEffect.m_sourceId, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
-				["sourceTarget"] = CGameEffect.m_sourceTarget, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
+				["sourceID"] = sprite.m_id,
+				["sourceTarget"] = sprite.m_id,
 			})
 		end
 	end
-end
+end)
