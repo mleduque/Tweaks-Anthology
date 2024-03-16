d22397cf19a535a966e89050455a2f29943c8a00
4Luke4 <luke9300@icloud.com>
2024-03-16 11:12:28 +0100

    Minor tweak

--- cdtweaks/luke/lua/dark_blessing.lua
+++ cdtweaks/luke/lua/dark_blessing.lua
@@ -38,39 +38,10 @@ EEex_Opcode_AddListsResolvedListener(function(sprite)
 	local spriteKitStr = GT_Resource_IDSToSymbol["kit"][EEex_BOr(EEex_LShift(sprite.m_baseStats.m_mageSpecUpperWord, 16), sprite.m_baseStats.m_mageSpecialization)]
 	--
 	local spriteFlags = sprite.m_baseStats.m_flags
-	-- Charisma => bonus
-	local darkBlessing = {
-		["0"] = -5,
-		["1"] = -5,
-		["2"] = -4,
-		["3"] = -4,
-		["4"] = -3,
-		["5"] = -3,
-		["6"] = -2,
-		["7"] = -2,
-		["8"] = -1,
-		["9"] = -1,
-		["10"] = 0,
-		["11"] = 0,
-		["12"] = 1,
-		["13"] = 1,
-		["14"] = 2,
-		["15"] = 2,
-		["16"] = 3,
-		["17"] = 3,
-		["18"] = 4,
-		["19"] = 4,
-		["20"] = 5,
-		["21"] = 5,
-		["22"] = 6,
-		["23"] = 6,
-		["24"] = 7,
-		["25"] = 7,
-	}
 	-- since ``EEex_Opcode_AddListsResolvedListener`` is running after the effect lists have been evaluated, ``m_bonusStats`` has already been added to ``m_derivedStats`` by the engine
 	local spriteCharisma = sprite.m_derivedStats.m_nCHR
 	--
-	local bonus = darkBlessing[string.format("%s", spriteCharisma)]
+	local bonus = math.floor((spriteCharisma - 10) / 2)
 	-- The blackguard adds its charisma bonus to all saving throws (provided it is not fallen)
 	local applyCondition = spriteClassStr == "PALADIN" and spriteKitStr == "Blackguard" and bonus and EEex_IsBitUnset(spriteFlags, 0x9)
 	--
