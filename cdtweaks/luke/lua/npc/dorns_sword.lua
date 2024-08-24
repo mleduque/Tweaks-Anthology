d9d4ade87c770566a6f0bfdc569f7d16c6ed91a8
4Luke4 <39967126+4Luke4@users.noreply.github.com>
2024-08-24 20:27:10 +0200

    Add missing INCLUDE

--- cdtweaks/luke/lua/npc/dorns_sword.lua
+++ cdtweaks/luke/lua/npc/dorns_sword.lua
@@ -2,17 +2,16 @@
 
 function GTDRNSW1(CGameEffect, CGameSprite)
 	local equipment = CGameSprite.m_equipment -- CGameSpriteEquipment
-	local selectedItem = equipment.m_items:get(equipment.m_selectedWeapon) -- CItem
-	local itemResRef = selectedItem.pRes.resref:get() -- We need to use :get() to export a CResRef field as a Lua string!
+	local selectedWeapon = equipment.m_items:get(equipment.m_selectedWeapon) -- CItem
+	local selectedWeaponResRef = selectedWeapon.pRes.resref:get() -- We need to use :get() to export a CResRef field as a Lua string!
 	-- if Dorn is not wielding Rancor +1, then remove bonus ...
-	if itemResRef ~= "SW2HD1" then
+	if selectedWeaponResRef ~= "SW2HD1" then
 		if EEex_Sprite_GetLocalInt(CGameSprite, "ohdornsw") ~= 0 then
 			EEex_Sprite_SetLocalInt(CGameSprite, "ohdornsw", 0)
 			--
 			EEex_GameObject_ApplyEffect(CGameSprite,
 			{
 				["effectID"] = 146, -- Cast spell
-				["durationType"] = 1,
 				["dwFlags"] = 1, -- Cast instantly (caster level)
 				["res"] = "OHDSW0",
 				["sourceID"] = CGameEffect.m_sourceId, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
