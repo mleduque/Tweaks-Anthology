3112add91e3aace1957ece86888a37a9cd82fec5
4Luke4 <luke9300@icloud.com>
2024-10-25 17:45:01 +0200

    Update dorns_sword.lua

--- cdtweaks/luke/lua/npc/dorns_sword.lua
+++ cdtweaks/luke/lua/npc/dorns_sword.lua
@@ -1,40 +1,44 @@
--- cdtweaks, Rancor +1 (Dorn's sword): remove thac0 bonus when unequipped --
+--[[
++------------------------------------+
+| cdtweaks, Rancor +1 (Dorn's sword) |
++------------------------------------+
+--]]
 
-function GTDRNSW1(CGameEffect, CGameSprite)
-	local equipment = CGameSprite.m_equipment -- CGameSpriteEquipment
-	local selectedWeapon = equipment.m_items:get(equipment.m_selectedWeapon) -- CItem
-	local selectedWeaponResRef = selectedWeapon.pRes.resref:get() -- We need to use :get() to export a CResRef field as a Lua string!
-	-- if Dorn is not wielding Rancor +1, then remove bonus ...
-	if selectedWeaponResRef ~= "SW2HD1" then
-		if EEex_Sprite_GetLocalInt(CGameSprite, "ohdornsw") ~= 0 then
-			EEex_Sprite_SetLocalInt(CGameSprite, "ohdornsw", 0)
-			--
+function GTSW2HD1(CGameEffect, CGameSprite)
+	if CGameEffect.m_effectAmount == 1 then
+		-- check if Dorn made the kill
+		local parentResRef = CGameEffect.m_sourceRes:get() -- We need to use :get() to export a CResRef field as a Lua string!
+		local spriteScriptName = CGameSprite.m_scriptName:get()
+		--
+		if string.upper(spriteScriptName) ~= "DORN" then
 			EEex_GameObject_ApplyEffect(CGameSprite,
 			{
-				["effectID"] = 146, -- Cast spell
-				["dwFlags"] = 1, -- Cast instantly (caster level)
-				["res"] = "OHDSW0",
+				["effectID"] = 206, -- Protection from spell
+				["effectAmount"] = -1, -- no feedback
+				["res"] = parentResRef,
 				["sourceID"] = CGameEffect.m_sourceId, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
 				["sourceTarget"] = CGameEffect.m_sourceTarget, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
 			})
 		end
-	end
-end
-
--- cdtweaks, Rancor +1 (Dorn's sword): Check if the wielder is Dorn --
-
-function GTDRNSW2(CGameEffect, CGameSprite)
-	local parentResRef = CGameEffect.m_sourceRes:get() -- We need to use :get() to export a CResRef field as a Lua string!
-	local spriteScriptName = EEex_CastUD(CGameSprite.m_scriptName, "CResRef"):get() -- certain engine types are nonsensical. We usually create fixups for the bindings whenever we run into them. We'll need to cast the value to properly read them
-	--
-	if string.upper(spriteScriptName) ~= "DORN" then
-		EEex_GameObject_ApplyEffect(CGameSprite,
-		{
-			["effectID"] = 206, -- Protection from spell
-			["effectAmount"] = -1, -- no feedback
-			["res"] = parentResRef,
-			["sourceID"] = CGameEffect.m_sourceId, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
-			["sourceTarget"] = CGameEffect.m_sourceTarget, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
-		})
+	elseif CGameEffect.m_effectAmount == 2 then
+		-- if Dorn is not wielding Rancor +1, then remove bonus ...
+		local equipment = CGameSprite.m_equipment -- CGameSpriteEquipment
+		local selectedWeapon = equipment.m_items:get(equipment.m_selectedWeapon) -- CItem
+		local selectedWeaponResRef = selectedWeapon.pRes.resref:get() -- We need to use :get() to export a CResRef field as a Lua string!
+		-- if Dorn is not wielding Rancor +1, then remove bonus ...
+		if string.upper(selectedWeaponResRef) ~= "SW2HD1" then
+			if EEex_Sprite_GetLocalInt(CGameSprite, "ohdornsw") ~= 0 then
+				EEex_Sprite_SetLocalInt(CGameSprite, "ohdornsw", 0)
+				--
+				EEex_GameObject_ApplyEffect(CGameSprite,
+				{
+					["effectID"] = 146, -- Cast spell
+					["dwFlags"] = 1, -- Cast instantly (caster level)
+					["res"] = "OHDSW0",
+					["sourceID"] = CGameEffect.m_sourceId, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
+					["sourceTarget"] = CGameEffect.m_sourceTarget, -- Certain opcodes (see f.i. op326) use this field internally... it's probably a good idea to always specify it...
+				})
+			end
+		end
 	end
 end
