abbc1f726ccfdd87905c02b7ae2df77a84f2a0b2
4Luke4 <luke9300@icloud.com>
2024-12-22 12:38:35 +0100

    Update infravision.lua
    Restrict to playable races only.

--- cdtweaks/luke/lua/rule_changes/infravision.lua
+++ cdtweaks/luke/lua/rule_changes/infravision.lua
@@ -6,7 +6,7 @@
 
 EEex_Opcode_AddListsResolvedListener(function(sprite)
 	-- Sanity check
-	if not EEex_GameObject_IsSprite(sprite) then
+	if not EEex_GameObject_IsSprite(sprite) or not sprite.m_pArea then
 		return
 	end
 	-- internal function that applies the actual penalty
@@ -33,6 +33,16 @@ EEex_Opcode_AddListsResolvedListener(function(sprite)
 			})
 		end
 	end
+	--
+	local playableRaces = {
+		["HUMAN"] = true,
+		["ELF"] = true,
+		["HALF_ELF"] = true,
+		["DWARF"] = true,
+		["GNOME"] = true,
+		["HALFLING"] = true,
+		["HALFORC"] = true,
+	}
 	-- Check creature's race / state
 	local spriteRaceStr = GT_Resource_IDSToSymbol["race"][sprite.m_typeAI.m_Race]
 	local racefeat = GT_Resource_2DA["racefeat"]
@@ -40,30 +50,32 @@ EEex_Opcode_AddListsResolvedListener(function(sprite)
 	-- since ``EEex_Opcode_AddListsResolvedListener`` is running after the effect lists have been evaluated, ``m_bonusStats`` has already been added to ``m_derivedStats`` by the engine
 	local spriteGeneralState = sprite.m_derivedStats.m_generalState
 	--
-	local isDungeon = sprite.m_pArea and EEex_IsBitSet(sprite.m_pArea.m_header.m_areaType, 0x5) or false
-	local isOutdoor = sprite.m_pArea and EEex_IsBitSet(sprite.m_pArea.m_header.m_areaType, 0x0) or false
-	local isDayNight = sprite.m_pArea and EEex_IsBitSet(sprite.m_pArea.m_header.m_areaType, 0x1) or false
+	local isDungeon = EEex_IsBitSet(sprite.m_pArea.m_header.m_areaType, 0x5)
+	local isOutdoor = EEex_IsBitSet(sprite.m_pArea.m_header.m_areaType, 0x0)
+	local isDayNight = EEex_IsBitSet(sprite.m_pArea.m_header.m_areaType, 0x1)
 	local isNight = EEex_Trigger_ParseConditionalString('TimeOfDay(NIGHT)')
 	--
 	local applyCondition = not (hasInnateInfravision or EEex_IsBitSet(spriteGeneralState, 17)) and (isDungeon or (isOutdoor and isDayNight and isNight:evalConditionalAsAIBase(sprite)))
 	--
-	if sprite:getLocalInt("cdtweaksNoInfravision") == 0 then
-		if applyCondition then
-			apply()
-		end
-	else
-		if applyCondition then
-			-- do nothing
+	if playableRaces[spriteRaceStr] then
+		if sprite:getLocalInt("cdtweaksNoInfravision") == 0 then
+			if applyCondition then
+				apply()
+			end
 		else
-			-- Mark the creature as 'condition removed'
-			sprite:setLocalInt("cdtweaksNoInfravision", 0)
-			--
-			sprite:applyEffect({
-				["effectID"] = 321, -- Remove effects by resource
-				["res"] = "GTRULE02",
-				["sourceID"] = sprite.m_id,
-				["sourceTarget"] = sprite.m_id,
-			})
+			if applyCondition then
+				-- do nothing
+			else
+				-- Mark the creature as 'condition removed'
+				sprite:setLocalInt("cdtweaksNoInfravision", 0)
+				--
+				sprite:applyEffect({
+					["effectID"] = 321, -- Remove effects by resource
+					["res"] = "GTRULE02",
+					["sourceID"] = sprite.m_id,
+					["sourceTarget"] = sprite.m_id,
+				})
+			end
 		end
 	end
 	--
